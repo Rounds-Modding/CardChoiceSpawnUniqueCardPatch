427354ffe71a0810dbf1f23f3215e273da7c1bc3
Root <Root@xn--36jc.st>
2023-08-24 19:09:30 -0400

    vertion number change

--- CardChoiceSpawnUniqueCardPatch/CardChoiceSpawnUniqueCardPatch.cs
+++ CardChoiceSpawnUniqueCardPatch/CardChoiceSpawnUniqueCardPatch.cs
@@ -17,7 +17,7 @@ namespace CardChoiceSpawnUniqueCardPatch
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "0.1.9")]
+    [BepInPlugin(ModId, ModName, "0.1.10")]
     [BepInProcess("Rounds.exe")]
     public class CardChoiceSpawnUniqueCardPatch : BaseUnityPlugin
     {
